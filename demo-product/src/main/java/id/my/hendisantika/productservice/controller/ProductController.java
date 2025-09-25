f049da875db602a673ecccf6d54fad446470a6a7
imgoby <imgoby@163.com>
2025-09-25 13:39:11 +0800

    新建项目

--- demo-product/src/main/java/id/my/hendisantika/productservice/controller/ProductController.java
+++ demo-product/src/main/java/id/my/hendisantika/productservice/controller/ProductController.java
@@ -1,20 +1,11 @@
 package id.my.hendisantika.productservice.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import id.my.hendisantika.productservice.model.Product;
 import id.my.hendisantika.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.List;
 
 
@@ -24,7 +15,6 @@ import java.util.List;
 public class ProductController {
 
     private final ProductRepository productRepository;
-    private final ObjectMapper mapper = new ObjectMapper();
 
     @PostMapping
     public Product add(@RequestBody Product product) {
@@ -42,9 +32,9 @@ public class ProductController {
     }
 
     @PostMapping("/products/ids")
-    public List<Product> find(@RequestBody List<Long> ids) throws JsonProcessingException {
+    public List<Product> find(@RequestBody List<Long> ids) {
         List<Product> products = productRepository.find(ids);
-        log.info("Products found: {}", mapper.writeValueAsString(Collections.singletonMap("count", products.size())));
+        log.info("Products found: {}", products);
         return products;
     }
 
